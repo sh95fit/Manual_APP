cbcfd80e8d8a91ce486da5bd12368f04867a66e1
sh95fit <h95fit@gmail.com>
2023-08-03 18:09:03 +0900

    검색 조건 설정 부분 js 작업 진행 중 (searchIndex 초기화 및 currentIndex 증가 부분 조치 필요)

--- manual_app/static/js/navbar.js
+++ manual_app/static/js/navbar.js
@@ -75,6 +75,299 @@ document.querySelector(".printer-btn").addEventListener("click", function () {
 
 ////////////////////////////////////////////////////////////////////////////////////
 
+// const searchInput = document.querySelector(".search-input");
+// const searchButton = document.querySelector(".search-button");
+// let contentIds = [];
+// let currentIndex = 0;
+
+// function performSearch() {
+//   const searchTerm = searchInput.value.trim().toLowerCase();
+//   if (searchTerm === "") return;
+
+//   fetch(`/search?term=${encodeURIComponent(searchTerm)}`)
+//     .then((response) => response.json())
+//     .then((data) => {
+//       if (data.length > 0) {
+//         contentIds = data.map((item) => `${item}`);
+//         console.log(contentIds);
+//         loadNextContent();
+//       } else {
+//         console.log("검색 결과가 없습니다.");
+//       }
+//     })
+//     .catch((error) => {
+//       console.error("Error performing search:", error);
+//     });
+// }
+
+// // 입력창에서 엔터 키 입력 이벤트 처리
+// searchInput.addEventListener("keydown", function (event) {
+//   if (event.key === "Enter") {
+//     event.preventDefault();
+//     performSearch();
+//   } else if (event.key === "Escape") {
+//     searchInput.value = ""; // 검색어를 초기화
+//     currentIndex = 0;
+//     contentIds = [];
+//   }
+// });
+
+// // 검색 버튼 클릭 이벤트 처리
+// searchButton.addEventListener("click", function () {
+//   performSearch();
+// });
+
+// function loadNextContent() {
+//   if (currentIndex >= contentIds.length) {
+//     currentIndex = 0;
+//   }
+
+//   const content = contentIds[currentIndex].replace(".html", "");
+//   const url = `/manual/${content}`;
+
+//   fetch(url)
+//     .then((response) => response.text())
+//     .then((data) => {
+//       $('.box-contents').html(data);
+//       scrollToSearchTerm();
+//     })
+//     .catch((error) => {
+//       console.error("Error loading content :", error);
+//     });
+
+//   currentIndex++;
+// }
+
+// function scrollToSearchTerm() {
+//   const searchTerm = searchInput.value.trim().toLowerCase();
+//   const searchTermElements = $('.box-contents').find(`*:contains('${searchTerm}')`);
+
+//   if (searchTermElements.length > 0) {
+//     const firstSearchTermElement = searchTermElements.eq(0);
+//     firstSearchTermElement[0].scrollIntoView({ behavior: "smooth" });
+//   }
+// }
+
+///////////////////////////////////
+
+// const searchInput = document.querySelector(".search-input");
+// const searchButton = document.querySelector(".search-button");
+// let contentIds = [];
+// let currentIndex = 0;
+
+// function performSearch() {
+//   const searchTerm = searchInput.value.trim().toLowerCase();
+//   if (searchTerm === "") return;
+
+//   fetch(`/search?term=${encodeURIComponent(searchTerm)}`)
+//     .then((response) => response.json())
+//     .then((data) => {
+//       if (data.length > 0) {
+//         contentIds = data.map((item) => `${item}`);
+//         console.log(contentIds);
+//         // currentIndex = 0; // 검색 결과를 초기화
+//         loadNextContent(); // 첫 번째 HTML 로드
+//       } else {
+//         console.log("검색 결과가 없습니다.");
+//       }
+//     })
+//     .catch((error) => {
+//       console.error("Error performing search:", error);
+//     });
+// }
+
+// // 입력창에서 엔터 키 입력 이벤트 처리
+// searchInput.addEventListener("keydown", function (event) {
+//   if (event.key === "Enter") {
+//     event.preventDefault();
+//     performSearch();
+//   } else if (event.key === "Escape") {
+//     searchInput.value = ""; // 검색어를 초기화
+//     currentIndex = 0;
+//     contentIds = [];
+//   }
+// });
+
+// // 검색 버튼 클릭 이벤트 처리
+// searchButton.addEventListener("click", function () {
+//   performSearch();
+// });
+
+// function loadNextContent() {
+//   if (currentIndex >= contentIds.length) {
+//     // 모든 검색 결과를 다 처리한 경우
+//     currentIndex = 0;
+//     return;
+//   }
+
+//   const content = contentIds[currentIndex].replace(".html", "");
+//   const url = `/manual/${content}`;
+
+//   fetch(url)
+//     .then((response) => response.text())
+//     .then((data) => {
+//       $('.box-contents').html(data);
+//       currentIndex++; // 다음 HTML로 넘어가기
+//       scrollToNextSearchTerm(); // 검색어별 스크롤 이동
+//     })
+//     .catch((error) => {
+//       console.error("Error loading content:", error);
+//     });
+// }
+
+// function scrollToNextSearchTerm() {
+//   const searchTerm = searchInput.value.trim().toLowerCase();
+//   if (searchTerm === "") return;
+
+//   const searchTermElements = $('.box-contents').find(`*:contains('${searchTerm}')`);
+
+//   if (searchTermElements.length > 0) {
+//     const firstSearchTermElement = searchTermElements.eq(0);
+//     firstSearchTermElement[0].scrollIntoView({ behavior: "smooth" });
+//   }
+// }
+
+
+//////////////////////////////////////////////////////////////////////////////////
+
+
+
+// // 검색어 입력란과 검색 버튼을 가져옵니다.
+// const searchInput = document.querySelector(".search-input");
+// const searchButton = document.querySelector(".search-button");
+// let contentIds = [];
+// let currentIndex = 0;
+// let lastSearchedTerm = "";
+
+// // 검색을 수행하는 함수
+// function performSearch() {
+//   // 검색어를 가져옵니다.
+//   const searchTerm = searchInput.value.trim().toLowerCase();
+//   if (searchTerm === "") return;
+
+//   // 이전 검색어와 다른 경우에만 초기화합니다.
+//   if (lastSearchedTerm !== searchTerm) {
+//     currentIndex = 0;
+//     contentIds = [];
+//     lastSearchedTerm = searchTerm;
+//   }
+
+//   // 서버로 검색 요청을 보냅니다.
+//   fetch(`/search?term=${encodeURIComponent(searchTerm)}`)
+//     .then((response) => response.json())
+//     .then((data) => {
+//       if (data.length > 0) {
+//         // 검색 결과가 있으면 contentIds에 저장하고 다음 검색 내용을 로드합니다.
+//         contentIds = data.map((item) => `${item}`);
+//         console.log(contentIds);
+//         loadNextContent();
+//       } else {
+//         console.log("검색 결과가 없습니다.");
+//       }
+//     })
+//     .catch((error) => {
+//       console.error("검색 중 오류 발생:", error);
+//     });
+// }
+
+// // 입력창에서 엔터 키 입력 이벤트 처리
+// searchInput.addEventListener("keydown", function (event) {
+//   if (event.key === "Enter") {
+//     event.preventDefault();
+//     moveToNextSearchTerm();
+//   } else if (event.key === "Escape") {
+//     searchInput.value = ""; // 검색어를 초기화
+//     currentIndex = 0;
+//     contentIds = [];
+//     lastSearchedTerm = "";
+//   }
+// });
+
+// // 검색 버튼 클릭 이벤트 처리
+// searchButton.addEventListener("click", function () {
+//   performSearch();
+// });
+
+// // 다음 검색어가 포함된 위치로 이동하는 함수
+// function moveToNextSearchTerm() {
+//   const searchTerm = searchInput.value.trim().toLowerCase();
+//   const searchTermElements = $('.box-contents').find(`*:contains('${searchTerm}')`);
+
+//   if (searchTermElements.length > 0) {
+//     // 현재 검색어의 위치 인덱스를 찾습니다.
+//     const currentIndex = searchTermElements.toArray().findIndex((element) => element.classList.contains("found-term"));
+
+//     if (currentIndex !== -1) {
+//       // 현재 검색어의 다음 위치로 이동합니다.
+//       const nextIndex = (currentIndex + 1) % searchTermElements.length;
+//       const nextElement = searchTermElements.get(nextIndex);
+//       nextElement.scrollIntoView({ behavior: "smooth" });
+//       nextElement.classList.add("found-term");
+//     } else {
+//       // 현재 페이지에서 검색어의 첫 위치로 이동합니다.
+//       const firstSearchTermElement = searchTermElements.get(0);
+//       firstSearchTermElement.scrollIntoView({ behavior: "smooth" });
+//       firstSearchTermElement.classList.add("found-term");
+//     }
+//   }
+// }
+
+// // 다음 내용을 로드하는 함수
+// function loadNextContent() {
+//   // 이전 검색어와 다른 경우에만 초기화합니다.
+//   if (lastSearchedTerm !== searchInput.value.trim().toLowerCase()) {
+//     currentIndex = 0;
+//     contentIds = [];
+//     lastSearchedTerm = searchInput.value.trim().toLowerCase();
+//   }
+
+//   if (currentIndex >= contentIds.length) {
+//     currentIndex = 0;
+//     lastSearchedTerm = "";
+//     return; // 모든 HTML 페이지에서 검색어의 모든 발생을 찾았습니다.
+//   }
+
+//   const content = contentIds[currentIndex].replace(".html", "");
+//   const url = `/manual/${content}`;
+
+//   fetch(url)
+//     .then((response) => response.text())
+//     .then((data) => {
+//       $('.box-contents').html(data);
+//       searchTermElements = []; // 새로운 컨텐츠를 로드할 때 검색어 위치를 다시 찾기 위해 초기화합니다.
+//       scrollToSearchTerm();
+//     })
+//     .catch((error) => {
+//       console.error("내용 로드 중 오류 발생:", error);
+//     });
+
+//   currentIndex++;
+// }
+
+// // 검색어가 있는 위치로 스크롤하는 함수
+// function scrollToSearchTerm() {
+//   const searchTerm = searchInput.value.trim().toLowerCase();
+//   searchTermElements = Array.from($('.box-contents').find(`*:contains('${searchTerm}')`));
+
+//   if (searchTermElements.length > 0) {
+//     // 검색어를 찾은 위치로 이동합니다.
+//     const firstSearchTermElement = searchTermElements[0];
+//     firstSearchTermElement.scrollIntoView({ behavior: "smooth" });
+//     firstSearchTermElement.classList.add("found-term");
+//   }
+// }
+
+
+
+
+
+
+
+
+
+
+
+
 const searchInput = document.querySelector(".search-input");
 const searchButton = document.querySelector(".search-button");
 let contentIds = [];
@@ -100,6 +393,80 @@ function performSearch() {
     });
 }
 
+function loadNextContent() {
+  if (currentIndex >= contentIds.length) {
+    currentIndex = 0;
+  }
+
+  const content = contentIds[currentIndex].replace(".html", "");
+  const url = `/manual/${content}`;
+
+  fetch(url)
+    .then((response) => response.text())
+    .then((data) => {
+      $('.box-contents').html(data);
+      findAndStoreSearchOccurrences();
+    })
+    .catch((error) => {
+      console.error("Error loading content :", error);
+    });
+
+  // currentIndex++;
+}
+
+
+
+
+
+
+let searchIndex = 0;
+
+function findAndStoreSearchOccurrences() {
+  const searchTerm = searchInput.value.trim().toLowerCase();
+  if (searchTerm === "") return;
+
+  const contentHTML = $('.box-contents').html();
+  const searchRegex = new RegExp(searchTerm, "gi");
+  let occurrences = [];
+  let match;
+
+  while ((match = searchRegex.exec(contentHTML)) !== null) {
+    const occurrence = {
+      position: match.index,
+      length: match[0].length,
+    };
+    occurrences.push(occurrence);
+  }
+
+  console.log("검색어 위치와 개수:", occurrences);
+  console.log(occurrences.length);
+  console.log(occurrences[searchIndex].position);
+  console.log(searchIndex);
+
+  if (occurrences.length > 0) {
+    if (searchIndex <= occurrences.length)
+      scrollToPosition(occurrences[searchIndex].position);
+      if (searchIndex >= occurrences.length) {
+        currentIndex++;
+        searchIndex = 0;
+      } else {
+        searchIndex++;
+      }
+  } else {
+    searchIndex = -1;
+  }
+}
+
+function scrollToPosition(position) {
+  window.scrollTo({ top: position, behavior: 'smooth' });
+}
+
+
+
+
+
+
+
 // 입력창에서 엔터 키 입력 이벤트 처리
 searchInput.addEventListener("keydown", function (event) {
   if (event.key === "Enter") {
@@ -116,23 +483,3 @@ searchInput.addEventListener("keydown", function (event) {
 searchButton.addEventListener("click", function () {
   performSearch();
 });
-
-function loadNextContent() {
-  if (currentIndex >= contentIds.length) {
-    currentIndex = 0;
-  }
-
-  const content = contentIds[currentIndex].replace(".html", "");
-  const url = `/manual/${content}`;
-
-  fetch(url)
-    .then((response) => response.text())
-    .then((data) => {
-      $('.box-contents').html(data);
-    })
-    .catch((error) => {
-      console.error("Error loading content :", error);
-    });
-
-  currentIndex++;
-}
